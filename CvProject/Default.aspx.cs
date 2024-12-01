4833bf9489d54ba2604a1c00fab02ac28774f7ed
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-12-01 18:09:59 +0300

    Ado Net

--- CvProject/Default.aspx.cs
+++ CvProject/Default.aspx.cs
@@ -5,13 +5,21 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 namespace CvProject
 {
-    public partial class Default : System.Web.UI.Page
+    public partial class _Default : System.Web.UI.Page
     {
+        DBCVENTITYEntities db = new DBCVENTITYEntities();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Repeater1.DataSource = db.TBLHAKKIMDAA.ToList();
+            Repeater1.DataBind();
+            Repeater2.DataSource = db.TBLHAKKIMDAA.ToList();
+            Repeater2.DataBind();
+            Repeater3.DataSource = db.TBLHAKKIMDAA.ToList();
+            Repeater3.DataBind();
         }
+
     }
 }
