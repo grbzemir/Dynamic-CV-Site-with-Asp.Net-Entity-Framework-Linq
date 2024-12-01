e8e79c517db9fbf24087115b14a091bb17a4886f
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-12-01 21:21:27 +0300

    Message

---
+++ CvProject/MesajDetay.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CvProject
+{
+    public partial class MesajDetay : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DBCVENTITYEntities db = new DBCVENTITYEntities();
+            int x = int.Parse(Request.QueryString["ID"]);
+            var mesaj = db.TBLILETISIM.Find(x);
+            TxtAdSoyad.Text= mesaj.ADSOYAD;
+            TxtKonu.Text = mesaj.KONU;
+            TxtMail.Text = mesaj.MAIL;
+            TxtMail.Text = mesaj.MESAJ;
+
+        }
+    }
+}
