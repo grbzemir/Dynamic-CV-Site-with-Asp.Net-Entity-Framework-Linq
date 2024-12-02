bae68f0eac716af3e57278134063b34f64ae6210
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-12-02 12:27:30 +0300

    YeniYetenek

--- CvProject/YeniYetenek.aspx.cs
+++ CvProject/YeniYetenek.aspx.cs
@@ -14,6 +14,7 @@ namespace CvProject
 
         }
 
+        //Database bağlantısı
         DBCVENTITYEntities db = new DBCVENTITYEntities();
         protected void Button1_Click(object sender, EventArgs e)
         {
