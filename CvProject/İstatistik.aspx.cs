663dfc727e8e81dc4ee36b647d0733646410719e
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-12-02 23:09:08 +0300

    New Dashboard

---
+++ CvProject/İstatistik.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CvProject
+{
+    public partial class İstatistik : System.Web.UI.Page
+    {
+        DBCVENTITYEntities db = new DBCVENTITYEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
